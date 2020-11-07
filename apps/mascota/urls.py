06f6ae1fbb19bb845713c55955969208d9200844
JesusRed <jesusalfredorv@ufps.edu.co>
2020-11-06 23:50:21 -0500

    table

--- apps/mascota/urls.py
+++ apps/mascota/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
-from apps.mascota.views import index_mascota
+from apps.mascota.views import index_mascota, mascota_view, mascota_list
 
 urlpatterns = [
-    url(r'^$', index_mascota),
+    url(r'^$', index_mascota, name="mascota_form"),
+    url(r'^nuevo/$', mascota_view, name="mascota_crear"),
+    url(r'^lsita/$', mascota_list, name="mascota_listar"),
 ]
