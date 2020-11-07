05924eeeef63081b27914dc5c4927c4e4b0e85f1
JesusRed <jesusalfredorv@ufps.edu.co>
2020-11-06 21:29:39 -0500

    upload

--- apps/adopcion/models.py
+++ apps/adopcion/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class Persona(models.Model):
 	nombre = models.CharField(max_length=50)
 	apellidos = models.CharField(max_length=70)
@@ -9,3 +10,4 @@ class Persona(models.Model):
 	telefono = models.CharField(max_length=12)
 	email = models.EmailField()
 	domicilio = models.TextField()
+	
