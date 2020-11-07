85bc0fa149d924059967ab8c2464c3b053a92bc2
JesusRed <jesusalfredorv@ufps.edu.co>
2020-11-07 00:00:37 -0500

    pylint

--- apps/mascota/urls.py
+++ apps/mascota/urls.py
@@ -4,5 +4,5 @@ from apps.mascota.views import index_mascota, mascota_view, mascota_list
 urlpatterns = [
     url(r'^$', index_mascota, name="mascota_form"),
     url(r'^nuevo/$', mascota_view, name="mascota_crear"),
-    url(r'^lsita/$', mascota_list, name="mascota_listar"),
+    url(r'^lista/$', mascota_list, name="mascota_listar"),
 ]
