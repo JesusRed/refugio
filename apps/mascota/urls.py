8858e8060bb4760f6f94bfc59777d44634bb6d94
JesusRed <jesusalfredorv@ufps.edu.co>
2020-11-10 13:56:47 -0500

    navbar links

--- apps/mascota/urls.py
+++ apps/mascota/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
+from django.contrib.auth.decorators import login_required
+
 from apps.mascota.views import index_mascota, mascota_view, mascota_list, mascota_edit, mascota_delete, MascotaList, MascotaCreate, MascotaUpdate, MascotDelete
 
 urlpatterns = [
     url(r'^$', index_mascota, name="mascota_form"),
-    url(r'^nuevo/$', MascotaCreate.as_view(), name="mascota_crear"),
-    url(r'^lista/$', MascotaList.as_view(), name="mascota_listar"),
-    url(r'^editar/(?P<pk>\d+)/$', MascotaUpdate.as_view(), name="mascota_editar"),
-    url(r'^eliminar/(?P<pk>\d+)/$', MascotDelete.as_view(), name="mascota_eliminar"),
+    url(r'^nuevo/$', login_required(MascotaCreate.as_view()), name="mascota_crear"),
+    url(r'^lista/$', login_required(MascotaList.as_view()), name="mascota_listar"),
+    url(r'^editar/(?P<pk>\d+)/$', login_required(MascotaUpdate.as_view()), name="mascota_editar"),
+    url(r'^eliminar/(?P<pk>\d+)/$', login_required(MascotDelete.as_view()), name="mascota_eliminar"),
 ]
--- refugio/settings.py
+++ refugio/settings.py
@@ -131,6 +131,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 LOGIN_REDIRECT_URL = reverse_lazy('adopcion:solicitud_listar')
+LOGOUT_REDIRECT_URL = reverse_lazy('login')
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
