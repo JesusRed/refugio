fe63a0442e0480dc39dbd4ef387f27e2a5d1ebcf
JesusRed <jesusalfredorv@ufps.edu.co>
2020-11-10 13:13:07 -0500

    forget pass

--- refugio/settings.py
+++ refugio/settings.py
@@ -131,3 +131,10 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 LOGIN_REDIRECT_URL = reverse_lazy('adopcion:solicitud_listar')
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 25
+EMAIL_HOST_USER = 'villalf19@gmail.com'
+EMAIL_HOST_PASSWORD = 'villalf19rojas'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
