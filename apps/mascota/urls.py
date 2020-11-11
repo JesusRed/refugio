1420657fc54d8556869c46f59b4e7c8cddc3bb48
JesusRed <jesusalfredorv@ufps.edu.co>
2020-11-10 21:16:12 -0500

    serialize

--- apps/mascota/urls.py
+++ apps/mascota/urls.py
@@ -1,12 +1,15 @@
 from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
 
-from apps.mascota.views import index_mascota, mascota_view, mascota_list, mascota_edit, mascota_delete, MascotaList, MascotaCreate, MascotaUpdate, MascotDelete
+from apps.mascota.views import listadousuarios, index_mascota, mascota_view, mascota_list, mascota_edit, mascota_delete, MascotaList, MascotaCreate, MascotaUpdate, MascotDelete
 
 urlpatterns = [
     url(r'^$', index_mascota, name="mascota_form"),
     url(r'^nuevo/$', login_required(MascotaCreate.as_view()), name="mascota_crear"),
     url(r'^lista/$', login_required(MascotaList.as_view()), name="mascota_listar"),
-    url(r'^editar/(?P<pk>\d+)/$', login_required(MascotaUpdate.as_view()), name="mascota_editar"),
-    url(r'^eliminar/(?P<pk>\d+)/$', login_required(MascotDelete.as_view()), name="mascota_eliminar"),
+    url(r'^editar/(?P<pk>\d+)/$',
+        login_required(MascotaUpdate.as_view()), name="mascota_editar"),
+    url(r'^eliminar/(?P<pk>\d+)/$',
+        login_required(MascotDelete.as_view()), name="mascota_eliminar"),
+    url(r'^listado/', listadousuarios, name="listado"),
 ]
--- apps/mascota/views.py
+++ apps/mascota/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.urls import reverse_lazy
+from django.core import serializers
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from apps.mascota.models import Mascota
 
 from apps.mascota.forms import MascotaForm
 from apps.mascota.models import Mascota
 # Create your views here.
+
+
+def listadousuarios(request):
+    lista = serializers.serialize('json', User.objects.all(), fields=['username', 'first_name'])
+    return HttpResponse(lista, content_type='application/json')
 
 
 def index_mascota(request):
@@ -23,10 +30,12 @@ def mascota_view(request):
         form = MascotaForm()
     return render(request, 'mascota/mascota_form.html', {'form': form})
 
+
 def mascota_list(request):
     mascota = Mascota.objects.all().order_by('id')
-    contexto = {'mascota':mascota}
+    contexto = {'mascota': mascota}
     return render(request, 'mascota/mascota_list.html', contexto)
+
 
 def mascota_edit(request, id_mascota):
     mascota = Mascota.objects.get(id=id_mascota)
@@ -36,20 +45,23 @@ def mascota_edit(request, id_mascota):
         form = MascotaForm(request.POST, instance=mascota)
         if form.is_valid():
             form.save()
-        return redirect ('mascota:mascota_listar')
-    return render(request, 'mascota/mascota_form.html', {'form':form})
+        return redirect('mascota:mascota_listar')
+    return render(request, 'mascota/mascota_form.html', {'form': form})
+
 
 def mascota_delete(request, id_mascota):
-    mascota = Mascota.objects.get(id = id_mascota)
+    mascota = Mascota.objects.get(id=id_mascota)
     if request.method == 'POST':
         mascota.delete()
         return redirect('mascota:mascota_listar')
-    return render(request, 'mascota/mascota_delete.html', {'mascota':mascota})
+    return render(request, 'mascota/mascota_delete.html', {'mascota': mascota})
+
 
 class MascotaList(ListView):
     model = Mascota
     template_name = 'mascota/mascota_list.html'
     queryset = Mascota.objects.order_by('id')
+
 
 class MascotaCreate(CreateView):
     model = Mascota
@@ -57,11 +69,13 @@ class MascotaCreate(CreateView):
     template_name = 'mascota/mascota_form.html'
     success_url = reverse_lazy('mascota:mascota_listar')
 
+
 class MascotaUpdate(UpdateView):
     model = Mascota
     form_class = MascotaForm
     template_name = 'mascota/mascota_form.html'
     success_url = reverse_lazy('mascota:mascota_listar')
+
 
 class MascotDelete(DeleteView):
     model = Mascota
